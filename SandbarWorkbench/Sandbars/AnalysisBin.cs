2e36018956a81f1d096e5b746e62837837255a92
Philip Bailey <philip@northarrowresearch.com>
2016-10-31 14:26:37 -0700

    stage discharge graph improvements

---
+++ SandbarWorkbench/Sandbars/AnalysisBin.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace SandbarWorkbench.Sandbars
+{
+    public class AnalysisBin
+    {
+        public long BinID { get; internal set; }
+        public string Title { get; internal set; }
+        public Nullable<double> LowerDischarge { get; internal set; }
+        public Nullable<double> UpperDischarge { get; internal set; }
+        public bool IsActive { get; internal set; }
+        public DateTime AddedOn { get; internal set; }
+        public string AddedBy { get; internal set; }
+        public DateTime UpdatedOn { get; internal set; }
+        public string UpdatedBy { get; internal set; }
+
+        public AnalysisBin(long nBinID, string sTitle, Nullable<double> fLD, Nullable<double> fUD, bool bIsActive, DateTime dtAddedOn, string sAddedBy, DateTime dtUpdatedOn, string sUpdatedBy)
+        {
+            BinID = nBinID;
+            Title = sTitle;
+            LowerDischarge = fLD;
+            UpperDischarge = fUD;
+            IsActive = bIsActive;
+            AddedOn = dtAddedOn;
+            AddedBy = sAddedBy;
+            UpdatedOn = dtUpdatedOn;
+            UpdatedBy = sUpdatedBy;
+        }
+
+        public static Dictionary<long, AnalysisBin> Load(string sDBCon)
+        {
+            Dictionary<long, AnalysisBin> dResult = new Dictionary<long, AnalysisBin>();
+
+            using (SQLiteConnection dbCon = new SQLiteConnection(sDBCon))
+            {
+                dbCon.Open();
+                SQLiteCommand dbCom = new SQLiteCommand("SELECT * FROM AnalysisBins ORDER BY LowerDischarge", dbCon);
+                SQLiteDataReader dbRead = dbCom.ExecuteReader();
+                while (dbRead.Read())
+                {
+                    Nullable<double> fLD = new Nullable<double>();
+                    if (!dbRead.IsDBNull(dbRead.GetOrdinal("LowerDischarge")))
+                        fLD = dbRead.GetDouble(dbRead.GetOrdinal("LowerDischarge"));
+
+                    Nullable<double> fUD = new Nullable<double>();
+                    if (!dbRead.IsDBNull(dbRead.GetOrdinal("UpperDischarge")))
+                        fUD = dbRead.GetDouble(dbRead.GetOrdinal("UpperDischarge"));
+
+                    dResult[dbRead.GetInt64(dbRead.GetOrdinal("BinID"))] = new AnalysisBin(
+                        dbRead.GetInt64(dbRead.GetOrdinal("BinID"))
+                        , dbRead.GetString(dbRead.GetOrdinal("Title"))
+                        , fLD
+                        , fUD
+                        , dbRead.GetBoolean(dbRead.GetOrdinal("IsActive"))
+                        , dbRead.GetDateTime(dbRead.GetOrdinal("AddedOn"))
+                        , dbRead.GetString(dbRead.GetOrdinal("AddedBy"))
+                        , dbRead.GetDateTime(dbRead.GetOrdinal("UpdatedOn"))
+                        , dbRead.GetString(dbRead.GetOrdinal("UpdatedBy"))
+                        );
+                }
+            }
+
+            return dResult;
+
+        }
+
+        /// <summary>
+        /// Get a list of all ACTIVE bin boundaries, both lower and upper, that have a value (i.e. ignoring the null lower and upper values)
+        /// </summary>
+        /// <param name="lBins"></param>
+        /// <returns></returns>
+        public static SortedList<double, double> GetActiveBinBoundaries(IEnumerable<AnalysisBin> lBins)
+        {
+            SortedList<double, double> dBins = new SortedList<double, double>();
+
+            foreach (AnalysisBin aBin in lBins)
+            {
+                if (aBin.IsActive)
+                {
+                    if (aBin.LowerDischarge.HasValue)
+                        if (!dBins.ContainsKey(aBin.LowerDischarge.Value))
+                            dBins.Add(aBin.LowerDischarge.Value, aBin.LowerDischarge.Value);
+
+                    if (aBin.UpperDischarge.HasValue)
+                        if (!dBins.ContainsKey(aBin.UpperDischarge.Value))
+                            dBins.Add(aBin.UpperDischarge.Value, aBin.UpperDischarge.Value);
+                }
+            }
+
+            return dBins;
+        }
+    }
+}
--- SandbarWorkbench/Sandbars/ucStageDischarge.cs
+++ SandbarWorkbench/Sandbars/ucStageDischarge.cs
@@ -13,7 +13,8 @@ namespace SandbarWorkbench.Sandbars
 {
     public partial class ucStageDischarge : UserControl
     {
-        public StageDischargeCurve SDCurve { get; set; }
+        public StageDischargeCurve SDCurve { get; internal set; }
+        public Dictionary<long, AnalysisBin> AnalysisBins { get; internal set; }
 
         public ucStageDischarge()
         {
@@ -22,7 +23,26 @@ namespace SandbarWorkbench.Sandbars
 
         private void ucStageDischarge_Load(object sender, EventArgs e)
         {
+            AnalysisBins = AnalysisBin.Load(DBCon.ConnectionStringLocal);
             LoadStageDischargeCurve();
+
+            chtData.ChartAreas[0].AxisX.Title = "Discharge (cfs)";
+            chtData.ChartAreas[0].AxisX.LabelStyle.Format = "#,##0";
+            chtData.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+            chtData.ChartAreas[0].AxisX.MinorGrid.Enabled = true;
+            chtData.ChartAreas[0].AxisX.MinorGrid.LineColor = Color.GhostWhite;
+            chtData.ChartAreas[0].AxisX.MinorTickMark.Enabled = true;
+            chtData.ChartAreas[0].AxisX.MinorTickMark.LineColor = Color.LightGray;
+
+            chtData.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chtData.ChartAreas[0].AxisY.MinorGrid.Enabled = true;
+            chtData.ChartAreas[0].AxisY.MinorGrid.LineColor = Color.GhostWhite;
+            chtData.ChartAreas[0].AxisY.MinorTickMark.Enabled = true;
+            chtData.ChartAreas[0].AxisY.MinorTickMark.LineColor = Color.LightGray;
+
+            chtData.ChartAreas[0].AxisY.Title = "Stage (ft)";
+
+
         }
 
         private void LoadStageDischargeCurve()
@@ -33,15 +53,43 @@ namespace SandbarWorkbench.Sandbars
             chtData.Series.Clear();
             Series theSeries = chtData.Series.Add("Stage Discharge");
             theSeries.ChartType = SeriesChartType.Line;
+            theSeries.IsVisibleInLegend = false;
 
+            Nullable<double> fMinStage = new Nullable<double>();
+            Nullable<double> fStage;
             if (SDCurve.HasAllValues)
             {
                 for (double fQ = 7500; fQ < 50000; fQ += 1000)
                 {
-                    theSeries.Points.AddXY(fQ, SDCurve.Stage(fQ));
+                    fStage = SDCurve.Stage(fQ);
+                    theSeries.Points.AddXY(fQ, fStage);
+
+                    if (fMinStage.HasValue)
+                        fMinStage = Math.Min(fStage.Value, fMinStage.Value);
+                    else
+                        fMinStage = fStage;
                 }
 
-                chtData.ChartAreas[0].AxisY.Minimum = 900;
+                chtData.ChartAreas[0].AxisY.Minimum = Math.Floor(fMinStage.Value / 10.0) * 10;
+
+                Series BinSeries = chtData.Series.Add("Analysis Bin Discharges");
+                BinSeries.IsVisibleInLegend = false;
+                BinSeries.ChartType = SeriesChartType.Point;
+                BinSeries.BorderWidth = 2;
+                BinSeries.MarkerSize = 10;
+                BinSeries.MarkerStyle = MarkerStyle.Circle;
+                BinSeries.Color = Color.Red;
+
+                SortedList<double, double> lBins = AnalysisBin.GetActiveBinBoundaries(AnalysisBins.Values);
+                foreach (double fBin in lBins.Values)
+                {
+                    fStage = SDCurve.Stage(fBin);
+                    if (fStage.HasValue)
+                    {
+                        int nPoint = BinSeries.Points.AddXY(fBin, fStage);
+                        BinSeries.Points[nPoint].Label = string.Format("{0:#,##0} cfs stage is {1:0.00}", fBin, fStage.Value);
+                    }
+                }
             }
         }
     }
