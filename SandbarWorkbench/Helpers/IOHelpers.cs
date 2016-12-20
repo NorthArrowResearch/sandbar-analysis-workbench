2fe3c28e7d370c72437a474bbfc4bf6237a124dc
Philip Bailey <philip@northarrowresearch.com>
2016-12-20 09:50:14 -0800

    moving IOHelpers to generic folder

--- SandbarWorkbench/Helpers/IOHelpers.cs
+++ SandbarWorkbench/Helpers/IOHelpers.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SandbarWorkbench.IOHelpers
+namespace SandbarWorkbench.Helpers
 {
     class IOHelpers
     {
